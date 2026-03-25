d36effd415825d9510ae735ea1fa68d48d17e49b
Tóth Zachary <231300066+zacharytoth@users.noreply.github.com>
2026-03-25 22:29:08 +0100

    Defining the headers and attributes for the buildings' classes

--- src/model/buildings/BusStop.java
+++ src/model/buildings/BusStop.java
@@ -1,4 +1,34 @@
 package model.buildings;
 
-public class BusStop {
+import java.util.List;
+
+import model.core.Game;
+import model.entities.Bus;
+import model.entities.Vehicle;
+
+public class BusStop extends Building{
+    /**
+     * The list of buses that has the BusStop as one of their stops
+     */
+    private List<Bus> buses;
+
+    /**
+     * A game object
+     */
+    private Game game;
+   
+    /**
+     * Checks, if it's the arriving bus' last stop
+     * If it is, it tells the shop to credit the money and increase the bus' finished rounds
+     * @param v The vehicle entering the busstop
+     */
+    public void enterVehicle(Vehicle v){}
+
+
+    /**
+     * It deployes the bus towards its destination.
+     * @param v The vehicle leaving the busstop.
+     */
+    public void deployVehicle(Vehicle v){}
+
 }
