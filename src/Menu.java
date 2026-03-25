87a303c9955a96f47f869336bc5057d38662bde6
Braun Norbert József <231318926+norbertjozsefbraun@users.noreply.github.com>
2026-03-26 00:37:43 +0100

    Create basic headers for Menu, Game, Session, Ticker

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,31 @@
 public class Menu {
+    /// base enum for state machine
+    enum States{
+        MAIN,
+        SETPLAYERS,
+        INGAME,
+        PREVIOuS
+    }
+
+    /// Fields:
+    private States state;
+
+    /// Getters:
+    public States getState() {
+        return state;
+    }
+
+    /// Setters:
+    public void setState(States state) {
+        this.state = state;
+    }
+
+    /// Functional functions:
+    public void listResults(){
+        //Todo: Implement this
+    }
+
+    public void start(){
+        //Todo: Implement this
+    }
 }
--- src/Ticker.java
+++ src/Ticker.java
@@ -1,2 +1,36 @@
 public class Ticker {
+    /// Fields
+    private Integer current;
+
+    /// Constructor
+    public Ticker(){
+        current = 0;
+    }
+
+    /// Getters:
+    public Integer getCurrent(){
+        return current;
+    }
+    /// Setters:
+    public void setCurrent(Integer current){
+        this.current = current;
+    }
+
+    /// Functional functions:
+
+    /*
+    Ticks the ticker by one unit.
+     */
+    public void tick() {
+        current++;
+    }
+
+    /*
+    Getter could be used as well, but due to the diagram and the avoidance of future
+    misunderstandings this function does the same as the getter.
+     */
+    public Integer getTick(){
+        return this.getCurrent();
+    }
+
 }
